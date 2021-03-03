87141ee24ffacf1209f07c2796218b98dbc094a8
mmmfang <mae.fang@gmail.com>
2021-03-03 03:24:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ function subtract(a, b) {
 
 function multiply(a, b) { return a * b};
 function divide(a, b) {return a/b};
+function increment (n) {
+  return n++;
+}
+function decrement(n) {return n--};
